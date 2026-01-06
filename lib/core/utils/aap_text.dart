e88cbf5cf9cc744d742076e3d796f32df8e854e0
NabilKhamu <nabilkhamis2n@gmail.com>
2026-01-06 20:26:53 +0200

    feat: add AppTexts constants for app strings

---
+++ lib/core/utils/aap_text.dart
@@ -0,0 +1,14 @@
+class AppTexts {
+  AppTexts._();
+
+  static const String appName = 'Delivery App';
+
+  static const String inicio = 'Inicio';
+  static const String searchHint = 'Buscar comida';
+  static const String exploreCategories = 'Explorar categorías';
+  static const String popularProducts = 'Productos populares';
+  static const String recommended = 'Recomendados';
+
+  static const String notifications = 'Notifications';
+  static const String seeAll = 'Ver todo';
+}
---
+++ lib/features/home/presentation/screens/home_screens.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/aap_text.dart';
+import '../widget/custom_appbar.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: HomeAppBar(
+        title: AppTexts.inicio,
+        onNotificationTap: () {},
+      ),
+      body: const SizedBox(),
+    );
+  }
+}
